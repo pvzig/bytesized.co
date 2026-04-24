63328bbba660948e570dd342a68a5c44a332750c
Peter Zignego <pzignego@gmail.com>
2026-04-23 21:45:21 -0400

    Revert "No more transparent bg param option"
    This reverts commit adabce18da85266013783355724e1a47acc3b48a.

--- Backend/Sources/Server/Model/ImageGenRequest.swift
+++ Backend/Sources/Server/Model/ImageGenRequest.swift
@@ -1,4 +1,5 @@
 struct ImageGenRequest: Encodable {
+    let background: String
     let model: String
     let outputFormat: String
     let prompt: String
@@ -6,6 +7,7 @@ struct ImageGenRequest: Encodable {
     let size: String
 
     enum CodingKeys: String, CodingKey {
+        case background
         case model
         case outputFormat = "output_format"
         case prompt
--- Backend/Sources/Server/OpenAiClient.swift
+++ Backend/Sources/Server/OpenAiClient.swift
@@ -21,6 +21,7 @@ struct OpenAiClient {
 
     func generateImage(prompt: String) async throws -> Data {
         let payload = ImageGenRequest(
+            background: "transparent",
             model: environment.openAIModel,
             outputFormat: "png",
             prompt: prompt,
